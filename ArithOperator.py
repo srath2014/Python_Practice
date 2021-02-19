0507d892aef1577e19c6b69c5abbdb9b6b9adb90
Soumyakanta <sikurath2014@gmail.com>
2021-02-19 10:23:27 +0530

    Arithmetic python program

---
+++ ArithOperator.py
@@ -0,0 +1,14 @@
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    sum = a + b
+    print(sum)
+
+    diff = a -b 
+    print(diff)
+
+    mult = a * b
+    print(mult)
+    2
